c7402dad4b70a328598dd48d5c31ad80f83f8a89
Waldo Heudens <waldo@heudens.be>
2022-11-30 00:55:26 +0100

    Add project files.

---
+++ Data/GroupSpace2022Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GroupSpace2022.Models;
+
+namespace GroupSpace2022.Data
+{
+    public class GroupSpace2022Context : DbContext
+    {
+        public GroupSpace2022Context (DbContextOptions<GroupSpace2022Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GroupSpace2022.Models.Group> Group { get; set; } = default!;
+    }
+}
---
+++ Models/Group.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupSpace2022.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Naam")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Omschrijving")]
+        public string Description { get; set; }
+
+        [Display(Name = "Gestart")]
+        [DataType (DataType.Date)]
+        public DateTime Started { get; set; }
+
+        [Display(Name = "Beëindigd")]
+        [DataType(DataType.Date)]
+        public DateTime Ended { get; set; }
+
+    }
+}
