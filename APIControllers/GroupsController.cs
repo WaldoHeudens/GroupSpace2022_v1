8bf4e314e63a3ef4bd5d6cae2bbcdd4b551f97d3
Waldo Heudens <waldo@heudens.be>
2023-01-09 16:09:09 +0100

    Statndaard API-controller

---
+++ APIControllers/GroupsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupSpace2022.Data;
+using GroupSpace2022.Models;
+using GroupSpace2022.Areas.Identity.Data;
+
+namespace GroupSpace2022.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly GroupSpace2022Context _context;
+
+        public GroupsController(GroupSpace2022Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetGroup()
+        {
+            return await _context.Group.ToListAsync();
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(int id)
+        {
+            Group @group = await _context.Group.FindAsync(id);
+
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return @group;
+        }
+
+        [HttpGet]
+        public  List<Group> GetUserGroup()
+        {
+            GroupSpace2022User _user = _context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+            List < UserGroup > usergroups = _context.UserGroup.Where(u => u.UserId == _user.Id).ToList();
+            List<Group> groups = new List<Group>();
+            foreach (UserGroup ug in usergroups)
+            {
+                groups.Add(_context.Group.FirstOrDefault(u => ug.GroupId == ug.GroupId));
+            }
+            return groups;
+        }
+
+        // PUT: api/Groups/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(int id, Group @group)
+        {
+            if (id != @group.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@group).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groups
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group @group)
+        {
+            _context.Group.Add(@group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGroup", new { id = @group.Id }, @group);
+        }
+
+        // DELETE: api/Groups/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroup(int id)
+        {
+            var @group = await _context.Group.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            _context.Group.Remove(@group);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GroupExists(int id)
+        {
+            return _context.Group.Any(e => e.Id == id);
+        }
+    }
+}
